a7e5342b4042fb3a9fccd6fd0f337f02f4a92ff1
Filip Tomes <filiptomes@protonmail.com>
2023-08-04 18:49:55 +0200

    fix: creating multiple prisma instancies

--- app/configs/db.config.ts
+++ app/configs/db.config.ts
@@ -1,3 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const db = globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db;

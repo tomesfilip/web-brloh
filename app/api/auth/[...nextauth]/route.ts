938fe17714f01c304b39c21cb7004224619b2388
Filip Tomes <filiptomes@protonmail.com>
2023-08-04 18:53:48 +0200

    creating NextAuth handler based on the auth options, exporting as GET and POST requests

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,5 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import NextAuth, { AuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcrypt';
-
-import { db } from '@/app/configs/db.config';
-import { getUser } from '../../services/user.service';
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(db),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error('Špatné přihlasovací údaje.');
-        }
-
-        const user = await getUser(undefined, credentials.email);
-        console.log(user);
-        if (!user || !user?.password) {
-          throw new Error('Špatné přihlasovací údaje.');
-        }
-
-        const match = await bcrypt.compare(credentials.password, user.password);
-        if (!match) {
-          throw new Error('Neplatné heslo.');
-        }
-
-        return user;
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/',
-  },
-  debug: process.env.NODE_ENV === 'development',
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-};
+import NextAuth from 'next-auth';
+import { authOptions } from '@/app/lib/auth';
 
 const handler = NextAuth(authOptions);
 
